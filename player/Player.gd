0eeea5f5815d1b54ee15949c989b27a36d1ab43a
vinibeloni <vinibelonicubas@gmail.com>
2023-10-30 00:08:32 -0300

    add animation movement

--- player/Player.gd
+++ player/Player.gd
@@ -1,13 +1,35 @@
 extends CharacterBody2D
 
+@onready var animation_tree = $AnimationTree
+
+var input = Vector2.ZERO
+
+func _ready():
+    animation_tree.active = true
+
 func _physics_process(delta):
-    var input = Vector2.ZERO;
-    input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
-    input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+    input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     
     if input != Vector2.ZERO:
         velocity = input
+        _set_walking(true)
+        _set_animations_position()
     else:
         velocity = Vector2.ZERO
+        _set_walking(false)
         
     move_and_collide(velocity)
+
+func _set_walking(value):
+    _set_animation_parameters("is_walking", value)
+    _set_animation_parameters("idle", not value)
+
+func _set_animations_position():
+  _set_blend_position("idle")
+  _set_blend_position("walk")
+
+func _set_blend_position(animationName: String) -> void:
+  animation_tree["parameters/{0}/blend_position".format([animationName])] = input
+
+func _set_animation_parameters(name: String, value: bool) -> void:
+    animation_tree["parameters/conditions/{0}".format([name])] = value
